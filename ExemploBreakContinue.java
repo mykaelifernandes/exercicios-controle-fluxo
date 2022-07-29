6d8bad536137b85303ea2228a0e8d7211fdb27b2
Mykaeli fernandes <88605247+mykaelifernandes@users.noreply.github.com>
2022-07-29 19:39:27 -0300

    Add files via upload

---
+++ ExemploBreakContinue.java
@@ -0,0 +1,13 @@
+public class ExemploBreakContinue {
+
+    public static void main(String[] args) {
+        for (int numero = 1; numero <=5; numero ++){
+            if (numero==3)
+                // break or continue;
+                // break para o for completamente.
+                // continue para apenas em algum momento.
+
+            System.out.println(numero);
+        }
+    }
+}
